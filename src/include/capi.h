0c25d745482fd4fea1979de5af7276c2547a9e0f
Denys Makogon <denys.makogon@oracle.com>
2022-12-07 19:16:47 +0200

    introducing more granular structs versioning

--- src/include/capi.h
+++ src/include/capi.h
@@ -1,12 +1,12 @@
 //
-//  2022.h
+//  capi.h
 //  src
 //
 //  Created by Denis Makogon on 05.08.2022.
 //
 
-#ifndef _022_h
-#define _022_h
+#ifndef _capi_h
+#define _capi_h
 
 #ifdef __cplusplus
 extern "C" {
@@ -48,4 +48,4 @@ extern "C" {
 }
 #endif
 
-#endif /* _022_h */
+#endif /* _capi_h */
