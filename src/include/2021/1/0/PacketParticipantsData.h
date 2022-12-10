30e94169e57cf0a85867e5f56c7bec58852300e4
Denys Makogon <denys.makogon@oracle.com>
2022-12-10 16:11:11 +0200

    remove duplicated unchanged structs in favour of dir symlinks

--- src/include/2021/1/0/PacketParticipantsData.h
+++
@@ -1,8 +0,0 @@
-struct PacketParticipantsData
-{
-    struct PacketHeader     m_header;           // Header
-
-    uint8                   m_numActiveCars;    // Number of active cars in the data – should match number of
-                                                // cars on HUD
-    struct ParticipantData m_participants[22];
-};
--- src/include/2022/1/13/PacketEventData.h
+++
@@ -1,8 +0,0 @@
-struct PacketEventData
-{
-    struct PacketHeader    	m_header;               // Header
-    
-    uint8           	    m_eventStringCode[4];   // Event string code, see below
-    union EventDataDetails	m_eventDetails;         // Event details - should be interpreted differently
-                                                    // for each type
-};
